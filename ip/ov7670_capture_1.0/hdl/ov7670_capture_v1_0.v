2afcccc742e03c669c9e60ebbee11d4caa096208
George Yu <35508393+georgeyhere@users.noreply.github.com>
2022-07-16 12:11:12 -0700

    retracked actual files

---
+++ ip/ov7670_capture_1.0/hdl/ov7670_capture_v1_0.v
@@ -0,0 +1,99 @@
+
+`timescale 1 ns / 1 ps
+
+	module ov7670_capture_v1_0 #
+	(
+		// Users to add parameters here
+        parameter X_RES = 640,
+        parameter Y_RES = 480,
+		// User parameters ends
+		// Do not modify the parameters beyond this line
+
+
+		// Parameters of Axi Slave Bus Interface S00_AXI
+		parameter integer C_S00_AXI_DATA_WIDTH	= 32,
+		parameter integer C_S00_AXI_ADDR_WIDTH	= 4
+	)
+	(
+		// Users to add ports here
+        input wire i_pclk,
+        input wire i_vsync,
+        input wire i_href,
+        input wire [7:0] i_data,
+
+        output wire [15:0] M_AXIS_VIDEO_TDATA,
+        output wire M_AXIS_TVALID,
+        input  wire M_AXIS_VIDEO_TREADY,
+        output wire M_AXIS_VIDEO_TUSER,
+        output wire M_AXIS_VIDEO_TLAST,
+		// User ports ends
+		// Do not modify the ports beyond this line
+
+
+		// Ports of Axi Slave Bus Interface S00_AXI
+		input wire  s00_axi_aclk,
+		input wire  s00_axi_aresetn,
+		input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_awaddr,
+		input wire [2 : 0] s00_axi_awprot,
+		input wire  s00_axi_awvalid,
+		output wire  s00_axi_awready,
+		input wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_wdata,
+		input wire [(C_S00_AXI_DATA_WIDTH/8)-1 : 0] s00_axi_wstrb,
+		input wire  s00_axi_wvalid,
+		output wire  s00_axi_wready,
+		output wire [1 : 0] s00_axi_bresp,
+		output wire  s00_axi_bvalid,
+		input wire  s00_axi_bready,
+		input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_araddr,
+		input wire [2 : 0] s00_axi_arprot,
+		input wire  s00_axi_arvalid,
+		output wire  s00_axi_arready,
+		output wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_rdata,
+		output wire [1 : 0] s00_axi_rresp,
+		output wire  s00_axi_rvalid,
+		input wire  s00_axi_rready
+	);
+// Instantiation of Axi Bus Interface S00_AXI
+	ov7670_capture_v1_0_S00_AXI # ( 
+        .X_RES (X_RES),
+        .Y_RES (Y_RES),
+		.C_S_AXI_DATA_WIDTH(C_S00_AXI_DATA_WIDTH),
+		.C_S_AXI_ADDR_WIDTH(C_S00_AXI_ADDR_WIDTH)
+	) ov7670_capture_v1_0_S00_AXI_inst (
+		.S_AXI_ACLK(s00_axi_aclk),
+		.S_AXI_ARESETN(s00_axi_aresetn),
+		.S_AXI_AWADDR(s00_axi_awaddr),
+		.S_AXI_AWPROT(s00_axi_awprot),
+		.S_AXI_AWVALID(s00_axi_awvalid),
+		.S_AXI_AWREADY(s00_axi_awready),
+		.S_AXI_WDATA(s00_axi_wdata),
+		.S_AXI_WSTRB(s00_axi_wstrb),
+		.S_AXI_WVALID(s00_axi_wvalid),
+		.S_AXI_WREADY(s00_axi_wready),
+		.S_AXI_BRESP(s00_axi_bresp),
+		.S_AXI_BVALID(s00_axi_bvalid),
+		.S_AXI_BREADY(s00_axi_bready),
+		.S_AXI_ARADDR(s00_axi_araddr),
+		.S_AXI_ARPROT(s00_axi_arprot),
+		.S_AXI_ARVALID(s00_axi_arvalid),
+		.S_AXI_ARREADY(s00_axi_arready),
+		.S_AXI_RDATA(s00_axi_rdata),
+		.S_AXI_RRESP(s00_axi_rresp),
+		.S_AXI_RVALID(s00_axi_rvalid),
+		.S_AXI_RREADY(s00_axi_rready),
+        .i_pclk  (i_pclk),
+        .i_vsync (i_vsync),
+        .i_href  (i_href),
+        .i_data  (i_data),
+        .M_AXIS_VIDEO_TDATA  (M_AXIS_VIDEO_TDATA),
+        .M_AXIS_TVALID       (M_AXIS_TVALID),
+        .M_AXIS_VIDEO_TREADY (M_AXIS_VIDEO_TREADY),
+        .M_AXIS_VIDEO_TUSER  (M_AXIS_VIDEO_TUSER),
+        .M_AXIS_VIDEO_TLAST  (M_AXIS_VIDEO_TLAST)
+	);
+
+	// Add user logic here
+
+	// User logic ends
+
+	endmodule
---
+++ ip/ov7670_capture_1.0/src/cam_capture_maxis.sv
@@ -0,0 +1,103 @@
+
+
+module cam_capture_maxis
+    #(parameter X_RES = 640,
+      parameter Y_RES = 480 )
+    (
+    input  logic        i_resetn, // async active-low reset
+    input  logic        i_enable,
+ 
+    // OV7670 interface  
+    input  logic        i_pclk,   // pixel clock 
+    input  logic        i_vsync,  // active-high vsync
+    input  logic        i_href,   // active-high href
+    input  logic [7:0]  i_data,   // 8-bit input data
+
+    // AXI-Stream Master Interface
+    output logic [15:0] M_AXIS_VIDEO_TDATA,
+    output logic        M_AXIS_TVALID,
+    input  logic        M_AXIS_VIDEO_TREADY, // unused
+    output logic        M_AXIS_VIDEO_TUSER,  // SoF
+    output logic        M_AXIS_VIDEO_TLAST   // EoL
+    );
+
+// LOCAL LOGIC
+    typedef enum {
+        ST_IDLE, ST_ACTIVE
+    } fsm_state_encoding;
+
+    fsm_state_encoding STATE;
+
+    logic [$clog2(X_RES)-1 :0] pixel_count;
+    logic [$clog2(Y_RES)-1 :0] row_count;
+    logic                      pixel_half;
+
+    logic                      vsync_q;
+    logic                      vsync_posedge;
+
+
+// VSYNC POSEDGE DETECTOR
+    always_ff@(posedge i_pclk or negedge i_resetn) begin 
+        if(!i_resetn) vsync_q <= 0;
+        else vsync_q <= i_vsync; 
+    end 
+    assign vsync_posedge = (!vsync_q) && i_vsync;
+
+// FSM
+    always_ff@(posedge i_pclk or negedge i_resetn) begin 
+        if(!i_resetn || !i_enable) begin 
+            M_AXIS_VIDEO_TDATA  <= 0;
+            M_AXIS_TVALID       <= 0;
+            M_AXIS_VIDEO_TUSER  <= 0;
+            M_AXIS_VIDEO_TLAST  <= 0;
+
+            STATE               <= ST_IDLE;
+            pixel_count         <= 0;
+            row_count           <= 0;
+            pixel_half          <= 0;
+        end 
+        else begin 
+            M_AXIS_VIDEO_TUSER <= (pixel_count == 0) && (row_count == 0);
+            M_AXIS_VIDEO_TLAST <= (pixel_count == X_RES-1);
+
+            case(STATE) 
+                ST_IDLE: begin 
+                    M_AXIS_TVALID <= 0;
+                    pixel_count   <= 0;
+                    row_count     <= 0;
+                    pixel_half    <= 0;
+                    if(vsync_posedge) STATE <= ST_ACTIVE;
+                end 
+
+                ST_ACTIVE: begin 
+                    if(!i_href) begin 
+                        M_AXIS_TVALID <= 0;
+                    end 
+                    else begin
+                        pixel_half <= !pixel_half;
+
+                        if(!pixel_half) begin
+                            M_AXIS_TVALID <= 0;
+                            M_AXIS_VIDEO_TDATA[7:0] <= i_data;
+                        end 
+                        else begin 
+                            M_AXIS_TVALID <= 1;
+                            M_AXIS_VIDEO_TDATA[15:8] <= i_data;
+
+                            if(pixel_count == X_RES-1) begin 
+                                pixel_count <= 0;
+                                if(row_count == Y_RES-1) STATE <= ST_IDLE; 
+                                else row_count <= row_count+1;  
+                            end 
+                            else begin 
+                                pixel_count <= pixel_count+1;
+                            end 
+                        end 
+                    end 
+                end 
+
+            endcase
+        end 
+    end 
+
+endmodule
