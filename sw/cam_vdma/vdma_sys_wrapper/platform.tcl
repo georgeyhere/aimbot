b44a6317f1986907de5cf0175ad10d953981a622
George Yu <georgeyhere@gmail.com>
2022-10-03 20:13:55 -0700

    add ov7670 driver test app

---
+++ sw/cam_vdma/vdma_sys_wrapper/platform.tcl
@@ -0,0 +1,21 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct /home/george/repos/aimbot/sw/cam_vdma/vdma_sys_wrapper/platform.tcl
+# 
+# OR launch xsct and run below command.
+# source /home/george/repos/aimbot/sw/cam_vdma/vdma_sys_wrapper/platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {vdma_sys_wrapper}\
+-hw {/home/george/repos/aimbot/sw/cam_vdma/vdma_sys_wrapper.xsa}\
+-out {/home/george/repos/aimbot/sw/cam_vdma}
+
+platform write
+domain create -name {standalone_ps7_cortexa9_0} -display-name {standalone_ps7_cortexa9_0} -os {standalone} -proc {ps7_cortexa9_0} -runtime {cpp} -arch {32-bit} -support-app {hello_world}
+platform generate -domains 
+platform active {vdma_sys_wrapper}
+domain active {zynq_fsbl}
+domain active {standalone_ps7_cortexa9_0}
+platform generate -quick
