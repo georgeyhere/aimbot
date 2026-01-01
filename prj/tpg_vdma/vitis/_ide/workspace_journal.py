fefeeb908d90b0bcf9c26cd809fab505a65afff0
George Yu <georgeyhere@gmail.com>
2026-01-01 15:30:08 -0800

    Add blank application project for app_tpg_vdma_demo

--- prj/tpg_vdma/vitis/_ide/workspace_journal.py
+++ prj/tpg_vdma/vitis/_ide/workspace_journal.py
@@ -6,3 +6,5 @@ client.set_workspace(path="vitis")
 
 platform = client.create_platform_component(name = "tpg_vdma_platform",hw_design = "$COMPONENT_LOCATION/../../tpg_vdma.xsa",os = "standalone",cpu = "ps7_cortexa9_0",domain_name = "standalone_ps7_cortexa9_0",compiler = "gcc")
 
+comp = client.create_app_component(name="app_tpg_vdma_demo",platform = "$COMPONENT_LOCATION/../tpg_vdma_platform/export/tpg_vdma_platform/tpg_vdma_platform.xpfm",domain = "standalone_ps7_cortexa9_0")
+
