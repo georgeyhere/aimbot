4dfa667e90bc0d6662ef22cb7776ec289ee91512
George Yu <georgeyhere@gmail.com>
2026-01-01 17:54:40 -0800

    Update readthedocs conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,13 +28,14 @@ author = 'George Yu'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+	'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = {'.rst': 'restructuredtext'}
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
